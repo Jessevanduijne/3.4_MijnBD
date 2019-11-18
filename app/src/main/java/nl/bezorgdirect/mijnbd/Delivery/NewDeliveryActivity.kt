1dc2e48d3ebb3cdef44c4790090bf212cfb54b7b
Jesse van Duijne <jessevanduijne@live.nl>
2019-11-19 00:15:22 +0100

    Edited incoming delivery layout, started to_warehouselayout

--- app/src/main/java/nl/bezorgdirect/mijnbd/Delivery/NewDeliveryActivity.kt
+++ app/src/main/java/nl/bezorgdirect/mijnbd/Delivery/NewDeliveryActivity.kt
@@ -1,24 +1,19 @@
 package nl.bezorgdirect.mijnbd.Delivery
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_choose_delivery.*
 import kotlinx.android.synthetic.main.toolbar.*
 import nl.bezorgdirect.mijnbd.R
 
-class ChooseDeliveryActivity : AppCompatActivity() {
+class NewDeliveryActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_choose_delivery)
+        setContentView(R.layout.activity_new_delivery)
 
         custom_toolbar_title.setText(getString(R.string.title_deliveries))
         setSupportActionBar(custom_toolbar)
 
-        btn_test_new_activity.setOnClickListener {
-            val intent = Intent(this@ChooseDeliveryActivity, NewDeliveryActivity::class.java)
-            startActivity(intent)
-        }
     }
 }
---
+++ app/src/main/java/nl/bezorgdirect/mijnbd/Delivery/ToWarehouseActivity.kt
@@ -0,0 +1,13 @@
+package nl.bezorgdirect.mijnbd.Delivery
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import nl.bezorgdirect.mijnbd.R
+
+class ToWarehouseActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_to_warehouse)
+    }
+}
