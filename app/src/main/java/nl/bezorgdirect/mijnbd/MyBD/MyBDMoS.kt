5f60be7a86b6294af6d829438f1176865221e4b6
Wessel <wesselkok@hotmail.com>
2019-11-19 11:10:23 +0100

    Added Info, Availabliity and Means of transport activities and progressed MyBD

--- app/src/main/java/nl/bezorgdirect/mijnbd/MyBD/MyBDMoS.kt
+++ app/src/main/java/nl/bezorgdirect/mijnbd/MyBD/MyBDMoS.kt
@@ -1,12 +1,13 @@
-package nl.bezorgdirect.mijnbd
+package nl.bezorgdirect.mijnbd.MyBD
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import nl.bezorgdirect.mijnbd.R
 
-class MyBDActivity : AppCompatActivity() {
+class MyBDMoS : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_my_bd)
+        setContentView(R.layout.activity_my_bdmo_s)
     }
 }
