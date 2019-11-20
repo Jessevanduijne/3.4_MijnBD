8223f948bac603fb275d75c08758e955db16f1fa
Jesse van Duijne <jessevanduijne@live.nl>
2019-11-20 14:47:19 +0100

    Some progress for the to_something layout

--- app/src/main/java/nl/bezorgdirect/mijnbd/Delivery/NewDeliveryActivity.kt
+++ app/src/main/java/nl/bezorgdirect/mijnbd/Delivery/NewDeliveryActivity.kt
@@ -1,8 +1,10 @@
 package nl.bezorgdirect.mijnbd.Delivery
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_choose_delivery.*
+import kotlinx.android.synthetic.main.activity_new_delivery.*
 import kotlinx.android.synthetic.main.toolbar.*
 import nl.bezorgdirect.mijnbd.R
 
@@ -15,5 +17,9 @@ class NewDeliveryActivity : AppCompatActivity() {
         custom_toolbar_title.setText(getString(R.string.title_deliveries))
         setSupportActionBar(custom_toolbar)
 
+        btn_accept_delivery.setOnClickListener {
+            val intent = Intent(this@NewDeliveryActivity, ToWarehouseActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
