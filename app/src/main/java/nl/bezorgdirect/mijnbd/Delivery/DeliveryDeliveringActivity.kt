50ad23307f19c980a87bb9f07ba9f7cc0f1ce4a7
Jesse van Duijne <jessevanduijne@live.nl>
2019-12-07 12:17:39 +0100

    Added functional google maps route

--- app/src/main/java/nl/bezorgdirect/mijnbd/Delivery/DeliveryDeliveringActivity.kt
+++ app/src/main/java/nl/bezorgdirect/mijnbd/Delivery/DeliveryDeliveringActivity.kt
@@ -1,14 +1,25 @@
 package nl.bezorgdirect.mijnbd.Delivery
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.maps.model.PolylineOptions
+import com.google.maps.android.PolyUtil
 import nl.bezorgdirect.mijnbd.R
+import nl.bezorgdirect.mijnbd.api.GoogleDirections
+import nl.bezorgdirect.mijnbd.api.GoogleService
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class DeliveryDeliveringActivity : AppCompatActivity(), OnMapReadyCallback {
 
@@ -20,15 +31,64 @@ class DeliveryDeliveringActivity : AppCompatActivity(), OnMapReadyCallback {
         val mapFragment = supportFragmentManager.findFragmentById(R.id.fragment_map) as SupportMapFragment
 
         mapFragment.getMapAsync(this)
+        getRoute()
     }
 
     override fun onMapReady(googleMap: GoogleMap?) {
         this.googleMap = googleMap
-
-        val latLngOrigin = LatLng(10.3181466, 123.9029382) // Ayala
-        val latLngDestination = LatLng(10.311795,123.915864) // SM City
-        this.googleMap!!.addMarker(MarkerOptions().position(latLngOrigin).title("Ayala"))
-        this.googleMap!!.addMarker(MarkerOptions().position(latLngDestination).title("SM City"))
+        val latLngOrigin = LatLng(52.3673469, 4.958289)
+        val latLngDestination = LatLng(52.3647484, 4.941496)
+        this.googleMap!!.addMarker(MarkerOptions().position(latLngOrigin).title("Thuis"))
+        this.googleMap!!.addMarker(MarkerOptions().position(latLngDestination).title("Coffeeshop"))
         this.googleMap!!.moveCamera(CameraUpdateFactory.newLatLngZoom(latLngOrigin, 14.5f))
+    }
+
+    private fun getRoute(){
+        val service = getGoogleService()
+        val path: MutableList<List<LatLng>> = ArrayList()
+
+        val startLatLong = "52.3673469,4.958289"
+        val endLatLong = "52.3647484,4.941496"
+        val travelmode = "cycling"
+
+        val apiKey = getString(R.string.google_maps_key)
+        val call = service.getDirections(startLatLong, endLatLong, apiKey, travelmode = travelmode)
+        call.enqueue(object: Callback<GoogleDirections> {
+
+            override fun onResponse(call: Call<GoogleDirections>, response: Response<GoogleDirections>) {
+                if(response.isSuccessful && response.body() != null) {
+                    val result = response.body()
+
+                    val routes = result?.routes
+                    val legs = routes?.get(0)?.legs
+                    val steps = legs?.get(0)?.steps
+                    for(i in 0 until steps!!.count()) {
+                        val polyline = steps[i].polyline
+                        val points = polyline.points
+                        path.add(PolyUtil.decode(points))
+                    }
+                    for(i in 0 until path.size) {
+                        googleMap!!.addPolyline(PolylineOptions().addAll(path[i]).color(Color.RED))
+                    }
+                }
+                else {
+                    Log.e("HTTP", "Google directions call unsuccessful")
+                }
+            }
+            override fun onFailure(call: Call<GoogleDirections>, t: Throwable) {
+                Log.e("HTTP", "Google directions call failed")
+            }
+        })
+    }
+
+    private fun getGoogleService(): GoogleService {
+
+        val retrofit = Retrofit.Builder()
+            .baseUrl("https://maps.googleapis.com/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+        val service = retrofit.create(GoogleService::class.java)
+        return service
     }
 }
---
+++ app/src/main/java/nl/bezorgdirect/mijnbd/api/GoogleService.kt
@@ -0,0 +1,18 @@
+package nl.bezorgdirect.mijnbd.api
+
+
+import nl.bezorgdirect.mijnbd.R
+import retrofit2.Call
+import retrofit2.http.*
+
+
+public interface GoogleService {
+
+    @GET("maps/api/directions/json")
+    fun getDirections(
+        @Query("origin") origin: String,
+        @Query("destination") destination: String,
+        @Query("key") key: String,
+        @Query("travelmode") travelmode: String
+    ): Call<GoogleDirections>
+}
