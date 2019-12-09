9bcabfe2d8d32b3ca3fa1d700c6f05de9075b909
Sam <laansam@live.nl>
2019-12-09 22:05:45 +0100

    navbar functionality

--- app/src/main/java/nl/bezorgdirect/mijnbd/Delivery/AssignmentActivity.kt
+++ app/src/main/java/nl/bezorgdirect/mijnbd/Delivery/AssignmentActivity.kt
@@ -1,18 +1,15 @@
 package nl.bezorgdirect.mijnbd.Delivery
 
-import android.annotation.TargetApi
-import android.app.*
-import android.content.Context
 import android.content.Intent
-import android.graphics.BitmapFactory
-import android.graphics.Color
-import android.os.Build
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.android.synthetic.main.activity_assignment.*
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import kotlinx.android.synthetic.main.bottom_bar.*
 import kotlinx.android.synthetic.main.toolbar.*
+import nl.bezorgdirect.mijnbd.History.MyBDHistory
 import nl.bezorgdirect.mijnbd.R.*
-import nl.bezorgdirect.mijnbd.R
 
 
 class AssignmentActivity : AppCompatActivity() {
@@ -25,6 +22,31 @@ class AssignmentActivity : AppCompatActivity() {
 
         custom_toolbar_title.setText(getString(string.title_assignment))
         setSupportActionBar(custom_toolbar)
+
+        bottom_navigation.setOnNavigationItemSelectedListener(object :
+            BottomNavigationView.OnNavigationItemSelectedListener {
+            override fun onNavigationItemSelected(item: MenuItem): Boolean {
+                when (item.getItemId()) {
+                    nl.bezorgdirect.mijnbd.R.id.action_history -> {
+                        //todo make history fragment
+                        //replaceFragment(R.id.delivery_fragment, assignment)
+                        val intent = Intent(this@AssignmentActivity, MyBDHistory::class.java)
+                        startActivity(intent)
+                    }
+                    nl.bezorgdirect.mijnbd.R.id.action_deliveries -> Toast.makeText(
+                        this@AssignmentActivity,
+                        "deliveries",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                    nl.bezorgdirect.mijnbd.R.id.action_mybd -> Toast.makeText(
+                        this@AssignmentActivity,
+                        "mybd",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+                return true
+            }
+        })
 
         val noAssignmentFragment = NoAssignmentFragment()
         supportFragmentManager.beginTransaction().replace(id.delivery_fragment, noAssignmentFragment).commit()
